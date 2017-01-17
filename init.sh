53262b04af6c4da32362291292eca073a4c4b79c
Esteban Murchio <esteban.murchio@gmail.com>
2017-01-17 11:06:28 -0300

    more styling and cosmetics...

--- init.sh
+++ init.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 clear
-printf "\ninitializing vimfresh"
 
 export HOME=~
+
 source "${HOME}/.vim/scripts/colors.sh"
 source "${HOME}/.vim/scripts/prompt.sh"
-source "${HOME}/.bashrc"
 
-printf "\ndone"
-printf "\n"
+source "${HOME}/.bashrc"
+#source "${HOME}/.vimrc"
+#exec "${HOME}/.screenrc"
+
+#exec screen
 
